063ae777da5a6da4138c2323ae7f6049eee771f8
DanishZDN <muhammad.danish@student.umn.ac.id>
2024-12-06 00:26:02 +0700

    Update on tutorial stage

---
+++ Assets/Specialthx.cs
@@ -0,0 +1,7 @@
+/*
+Thank you to the following asset that was used in the creation of this project:
+https://assetstore.unity.com/packages/3d/environments/maze-modular-puzzle-kit-302221
+https://assetstore.unity.com/packages/2d/textures-materials/floors/hand-painted-stone-texture-73949
+https://assetstore.unity.com/packages/3d/hand-painted-wooden-crate-36854
+https://assetstore.unity.com/packages/3d/props/stylized-fantasy-props-sample-234139
+*/
