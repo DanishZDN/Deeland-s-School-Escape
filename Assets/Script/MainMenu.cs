ba937eed2085c6e07d614ec231537582c1de2c5c
DanishZDN <muhammad.danish@student.umn.ac.id>
2024-12-11 16:05:51 +0700

    Lobby fix and animation fix

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -3,12 +3,41 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    public enum ButtonAction
     {
-        SceneManager.LoadScene("Tutorial"); 
+        PlayGame,
+        QuitGame
     }
 
-    public void QuitGame()
+    public ButtonAction action;
+
+    public void OnButtonClick()
+    {
+        switch (action)
+        {
+            case ButtonAction.PlayGame:
+                PlayGame();
+                break;
+            case ButtonAction.QuitGame:
+                QuitGame();
+                break;
+        }
+    }
+
+    private void PlayGame()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        if (currentScene.name == "MainMenu")
+        {
+            SceneManager.LoadScene("Tutorial");
+        }
+        else if (currentScene.name == "Tutorial")
+        {
+            SceneManager.LoadScene("NextStage");
+        }
+    }
+
+    private void QuitGame()
     {
         Application.Quit();
     }
