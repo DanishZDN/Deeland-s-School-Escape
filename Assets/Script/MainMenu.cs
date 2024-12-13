bb59316c6a034760bb45e44a32fa7427ccb43aec
DanishZDN <muhammad.danish@student.umn.ac.id>
2024-12-13 23:14:12 +0700

    MAIN MENU BERHASIL YES!

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -3,42 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public enum ButtonAction
+    public void Play()
     {
-        PlayGame,
-        QuitGame
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);  
     }
 
-    public ButtonAction action;
-
-    public void OnButtonClick()
-    {
-        switch (action)
-        {
-            case ButtonAction.PlayGame:
-                PlayGame();
-                break;
-            case ButtonAction.QuitGame:
-                QuitGame();
-                break;
-        }
-    }
-
-    private void PlayGame()
-    {
-        Scene currentScene = SceneManager.GetActiveScene();
-        if (currentScene.name == "MainMenu")
-        {
-            SceneManager.LoadScene("Tutorial");
-        }
-        else if (currentScene.name == "Tutorial")
-        {
-            SceneManager.LoadScene("NextStage");
-        }
-    }
-
-    private void QuitGame()
+    public void Quit()
     {
         Application.Quit();
+        Debug.Log("Player has Quit");  
     }
 }
